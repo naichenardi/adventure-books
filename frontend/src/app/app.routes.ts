32c21e643200cdd87b607b2f2ada513528ebff2c
naiche <naichenardi@gmail.com>
2026-08-19 10:56:58 +0100

    feat: wire up app shell, routing, and HTTP client for adventure pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import {Routes} from '@angular/router';
+import {GamePageComponent} from './pages/game-page/game-page.component';
+import {LibraryPageComponent} from './pages/library-page/library-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: LibraryPageComponent
+	},
+	{
+		path: 'play',
+		component: GamePageComponent
+	},
+	{
+		path: 'play/:bookId',
+		component: GamePageComponent
+	},
+	{
+		path: '**',
+		redirectTo: ''
+	}
+];
